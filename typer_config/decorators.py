3ab6ec58769ea434a20f4e4731cae9226bd300fe
Matt Anderson <matt@manderscience.com>
2023-08-25 20:08:37 -0500

    chore: coverage fix

--- typer_config/decorators.py
+++ typer_config/decorators.py
@@ -25,7 +25,7 @@ from .loaders import (
     yaml_loader,
 )
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from .__typing import (
         ConfigDumper,
         ConfigParameterCallback,
